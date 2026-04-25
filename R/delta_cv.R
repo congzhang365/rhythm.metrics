f2582bee0201a1aed3f9d50425ec7d476e0a3770
Cong Zhang <rolin365@gmail.com>
2026-04-25 02:27:04 +0100

    Refactor delta_cv function and documentation
    Updated author email and parameter descriptions for clarity. Changed function implementation to use curly-curly operator for dplyr compatibility.

--- R/delta_cv.R
+++ R/delta_cv.R
@@ -7,36 +7,38 @@
 #' `Delta C: SD of total C duration`
 #' `Delta V: SD of total V duration`
 #'
-#' @author Cong Zhang, \email{cong.zhang@ru.nl}
-#' @param
-#' df: a data frame containing cv_labels, utterance_id, and cv_duration values.
-#' @param
-#' cv_label: whether a segment is a C(onsonant) or a V(owel)
-#' @param
-#' utterance_id: each unique utterance should have a unique id
-#' @param
-#' cv_duration: the duration of C or V
+#' @author Cong Zhang, \email{cong.zhang@newcastle.ac.uk}
+#' @param df a data frame containing cv_labels, utterance_id, and cv_duration values.
+#' @param cv_label the column name for whether a segment is a C(onsonant) or a V(owel)
+#' @param utterance_id the column name where each unique utterance has a unique id
+#' @param cv_duration the column name for the duration of C or V
 #'
-#' @return
-#' delta_cv: a data frame containing the results of Delta C and Delta V values
+#' @return a data frame containing the results of Delta C and Delta V values
 #' @examples
-#' df <- data.frame (cv_label  = c("c", "v", "c", "v","c", "v", "c", "v"),
-#'                   utterance_id = c("utt_1", "utt_1", "utt_2", "utt_2",
-#'                                    "utt_1", "utt_1", "utt_2", "utt_2"),
-#'                   cv_duration = c(0.1, 0.8, 0.2, 0.5, 0.3, 0.3, 0.4, 0.7))
+#' df_test <- data.frame(cv_label  = c("c", "v", "c", "v","c", "v", "c", "v"),
+#'                       utterance_id = c("utt_1", "utt_1", "utt_2", "utt_2",
+#'                                        "utt_1", "utt_1", "utt_2", "utt_2"),
+#'                       cv_duration = c(0.1, 0.8, 0.2, 0.5, 0.3, 0.3, 0.4, 0.7))
 #'
-#' delta_cv(df, df$cv_label, df$utterance_id, df$cv_duration)
+#' delta_cv(df_test, cv_label, utterance_id, cv_duration)
 #'
+#' @importFrom magrittr %>%
+#' @importFrom stats sd
+#' @importFrom dplyr group_by summarise
+#' @importFrom rlang {{ }}
 #' @export
 delta_cv <- function(df, cv_label, utterance_id, cv_duration) {
-  delta_cv <- df %>%
-    dplyr::group_by(cv_label, utterance_id) %>%
-    dplyr::summarise(mean_d = sd(cv_duration, na.rm = T))
+  
+  # We use the {{ }} (curly-curly) operator for compatibility with 
+  # modern dplyr inside functions, but the pipe remains the classic %>%
+  res <- df %>%
+    dplyr::group_by({{ cv_label }}, {{ utterance_id }}) %>%
+    dplyr::summarise(mean_d = stats::sd({{ cv_duration }}, na.rm = TRUE), .groups = "drop")
 
-  delta_cv <- delta_cv %>%
-    dplyr::group_by(cv_label) %>%
-    dplyr::summarise(mean_delta = mean(mean_d, na.rm = T),
-                     sd_delta = sd(mean_d, na.rm = T))
+  res <- res %>%
+    dplyr::group_by({{ cv_label }}) %>%
+    dplyr::summarise(mean_delta = mean(mean_d, na.rm = TRUE),
+                     sd_delta = stats::sd(mean_d, na.rm = TRUE))
 
-  delta_cv
+  return(res)
 }
